0bd132cd0646262ebe9f972b2b83596e1d01e0c2
C0d3president <kadimasam@gmail.com>
2021-08-13 14:04:28 +0300

    Add a test to verify the existence of endangered animal class

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class EndangeredAnimalTest {
+
+    @Test
+    public void EndangeredAnimalClassExists(){
+        EndangeredAnimal endangeredAnimal = new EndangeredAnimal();
+        Assertions.assertEquals(true, endangeredAnimal instanceof EndangeredAnimal);
+    }
 }
