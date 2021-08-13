4bc4d8f10da24ec93a5d01710825c42ded0960c1
C0d3president <kadimasam@gmail.com>
2021-08-13 15:14:54 +0300

    Add getter and setter methods for endagered animal class variables

--- src/main/java/modules/EndangeredAnimal.java
+++ src/main/java/modules/EndangeredAnimal.java
@@ -7,14 +7,46 @@ public class EndangeredAnimal extends Animal{
     private int id;
     private String health;
     private String age;
-    public ArrayList<EndangeredAnimal> endangeredAnimals = new ArrayList<>();
+    public static ArrayList<EndangeredAnimal> endangeredAnimals = new ArrayList<>();
 
 
     public EndangeredAnimal(String name,String health, String age) {
         super(name);
         this.health = health;
         this.age = age;
-        this.id = endangeredAnimals.getSize();
+        this.id = id;
     }
 
+    @Override
+    public int getId() {
+        return endangeredAnimals.size();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public static ArrayList<EndangeredAnimal> getEndangeredAnimals() {
+        return endangeredAnimals;
+    }
+
+    public void setEndangeredAnimals(ArrayList<EndangeredAnimal> endangeredAnimals) {
+        this.endangeredAnimals = endangeredAnimals;
+    }
 }
